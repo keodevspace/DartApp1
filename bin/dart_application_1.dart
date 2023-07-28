78522de8085a74ddd1eaffb552d4e3d8db56f827
KEO <keoxcoelho@gmail.com>
2023-07-28 11:16:14 -0300

    created

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,17 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main() {
+  String nome = "Laranja";
+  double peso= 0.5;
+  String cor = "Amarela";
+  String sabor = "Doce e Cítrica";
+  int diasDesdeColheita = 10;
+  bool isMadura;
+
+if (diasDesdeColheita >= 5){
+  isMadura = true;
+} else {
+  isMadura = false;
+}
+
+}
